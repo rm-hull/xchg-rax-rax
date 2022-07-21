f5fb7fb6d30265daba61191a333c74e9feee0546
Richard Hull <rm_hull@yahoo.co.uk>
2022-07-21 17:52:44 +0100

    Update default font; Add line numbers to editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,23 +1,31 @@
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-nasm";
-import "prismjs/themes/prism-dark.css"; //Example style, you can use another
+import "prismjs/themes/prism-dark.css";
 import Editor from "react-simple-code-editor";
+import "./styles.css";
 
 type EditorProps = {
   code: string;
   onCodeChange: (code: string) => void;
 };
 
+const hightlightWithLineNumbers = (input: string, language: string): string =>
+  highlight(input, language)
+    .split("\n")
+    .map((line: string, i: number) => `<span class='editorLineNumber'>${i + 1}</span>${line}`)
+    .join("\n");
+
 export function CodeEditor({ code, onCodeChange }: EditorProps) {
   return (
     <Editor
       value={code}
       onValueChange={onCodeChange}
-      highlight={(code) => highlight(code, languages.nasm)}
+      highlight={(code) => hightlightWithLineNumbers(code, languages.nasm)}
       padding={10}
+      textareaId="codeArea"
+      className="editor"
       style={{
-        // eslint-disable-next-line quotes
-        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontFamily: "JetBrainsMono, monospace",
         fontSize: 12,
       }}
     />
