1ff7ee43ad1afeb137c51f9255e59c5003558442
Richard Hull <rm_hull@yahoo.co.uk>
2022-07-21 22:38:11 +0100

    Add current line number background

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-nasm";
 import "prismjs/themes/prism-dark.css";
@@ -9,11 +10,16 @@ type EditorProps = {
   onCodeChange: (code: string) => void;
 };
 
-const hightlightWithLineNumbers = (input: string, language: string): string =>
+const hightlightWithLineNumbers = (input: string, language: string, currentLine?: number): JSX.Element =>
   highlight(input, language)
     .split("\n")
-    .map((line: string, i: number) => `<span class='editorLineNumber'>${i + 1}</span>${line}`)
-    .join("\n");
+    .map((line: string, i: number) => (
+      <div key={i} className={clsx({ currentLine: currentLine === i })}>
+        <span className="editorLineNumber">{i + 1}</span>
+        <span dangerouslySetInnerHTML={{ __html: line }} />
+        <br />
+      </div>
+    ));
 
 export function CodeEditor({ code, onCodeChange }: EditorProps) {
   return (
